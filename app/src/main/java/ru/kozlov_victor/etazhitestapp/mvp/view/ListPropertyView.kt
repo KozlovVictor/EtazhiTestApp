a02dcdf866815a4cff5f546462f93e951b92ca52
Admin <hostelv@list.ru>
2019-03-28 16:18:42 +0600

    control point

---
+++ app/src/main/java/ru/kozlov_victor/etazhitestapp/mvp/view/ListPropertyView.kt
@@ -0,0 +1,9 @@
+package ru.kozlov_victor.etazhitestapp.mvp.view
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(value = AddToEndSingleStrategy::class)
+interface ListPropertyView : MvpView {
+}
